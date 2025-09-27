e4d5cdb63e2fb3832438c5f76030eff3c1dfb9bb
Jonathon Robel <jonrrobel@gmail.com>
2025-09-26 19:38:06 -0600

    Hello World http test

--- main.go
+++ main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
 
 func main() {
 	fmt.Println("Hello world!")
+
+	handler := func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+
+		fmt.Fprintf(w, "hello world")
+	}
+
+	http.HandleFunc("/", handler)
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		fmt.Printf("ListenAndServe: %v", err)
+	}
 }
