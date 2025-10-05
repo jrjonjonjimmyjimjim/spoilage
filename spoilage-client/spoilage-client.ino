e0ebbcfdd48bc73ec2225ee5bf62816d8f7d5183
Jonathon Robel <jonrrobel@gmail.com>
2025-10-05 12:28:56 -0600

    Arduino fetches summary and displays it

--- spoilage-client/spoilage-client.ino
+++ spoilage-client/spoilage-client.ino
@@ -9,6 +9,15 @@
 char ssid[] = SECRET_SSID;
 char pass[] = SECRET_PASS;
 char server[] = "budgeting.robel.dev";
+const int serverPort = 8443;
+const int MAX_ITEM_COUNT = 32;
+
+typedef struct Item{
+  int Id;
+  String Name;
+  int DaysLeft;
+};
+struct Item itemsList[MAX_ITEM_COUNT] = {};
 
 int wifiStatus = WL_IDLE_STATUS;
 const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
@@ -17,7 +26,6 @@ LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 WiFiSSLClient client;
 
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(115200);
   while (!Serial) {
     ;
@@ -26,7 +34,15 @@ void setup() {
   lcd.begin(20, 4);
   lcd.print("SPOILAGE v0.1.0");
   lcd.setCursor(0, 1);
-  lcd.print("Connecting to Wi-Fi");
+  lcd.print("Network: ");
+  lcd.setCursor(9, 1);
+  lcd.print(ssid);
+  lcd.setCursor(0, 2);
+  lcd.print(server);
+  lcd.setCursor(0, 3);
+  lcd.print("Port: ");
+  lcd.setCursor(6, 3);
+  lcd.print(serverPort);
 
   pinMode(LED_BUILTIN, OUTPUT);
   pinMode(8, OUTPUT);
@@ -60,12 +76,48 @@ void loop() {
   JsonDocument doc;
   apiRequest("GET /api/summary", "", doc);
 
-  Serial.println((String)(doc[0]["item_name"]));
-  /*
-  for (JsonVariant item : doc) {
-    Serial.print(item["item_id"]); Serial.print(item["item_name"]); Serial.print(item["expires"]); Serial.println(item["days_till_expiration"]);
+  JsonArray items = doc["items"];
+  Serial.println(String(items[0]["item_name"]));
+
+  int itemIndex = 0;
+  for (JsonVariant item : items) {
+    itemsList[itemIndex].Id = item["item_id"];
+    itemsList[itemIndex].Name = String(item["item_name"]);
+    itemsList[itemIndex].DaysLeft = item["days_till_expiration"];
+    Serial.println("Adding entry to itemsList");
+    Serial.println(itemsList[itemIndex].Id);
+    Serial.println(itemsList[itemIndex].Name);
+    Serial.println(itemsList[itemIndex].DaysLeft);
+
+    if (++itemIndex >= MAX_ITEM_COUNT) {
+      Serial.println("Received more item entries than could fit.");
+      break;
+    }
   }
-  */
+  // Overwrite the Id of any remaining entries in the array
+  for (int i = itemIndex; i < MAX_ITEM_COUNT; i++) {
+    itemsList[itemIndex].Id = 0;
+  }
+
+  lcd.clear();
+  for (int i = 0; i < 4; i++) {
+    if (itemsList[i].Id == 0) {
+      break;
+    }
+    lcd.setCursor(0, i);
+    lcd.write(itemsList[i].Name.c_str());
+    String daysLeftString = String(itemsList[i].DaysLeft);
+    Serial.println(itemsList[i].DaysLeft);
+    Serial.println(daysLeftString.length());
+    lcd.setCursor(19-daysLeftString.length()-1, i);
+    lcd.write(" ");
+    lcd.setCursor(19-daysLeftString.length(), i);
+    lcd.write(daysLeftString.c_str());
+    lcd.setCursor(19, i);
+    lcd.write("d");
+  }
+
+  for (;;) {}
 
   if (!client.connected()) {
     Serial.println("Disconnecting from server");
@@ -75,12 +127,17 @@ void loop() {
   }
 }
 
-// TODO: This should be adapted to return a variable length string of the response body.
-// It should also take in the "GET /api/summary" part of the request and handle making the connection.
-void apiRequest(char endpoint[], char body[], JsonDocument doc) {
+void apiRequest(char endpoint[], char body[], JsonDocument &doc) {
+  while (client.connected()) {
+    Serial.println("Waiting for previous connection to close");
+    digitalWrite(LED_BUILTIN, HIGH);
+    delay(100);
+    digitalWrite(LED_BUILTIN, LOW);
+    delay(500);
+  }
   Serial.print("\nCalling server: "); Serial.println(endpoint);
   Serial.println(body);
-  if (client.connect(server, 8443)) {
+  if (client.connect(server, serverPort)) {
     Serial.println("Connected to server");
     client.print(endpoint); client.println(" HTTP/1.1");
     client.println("Host: budgeting.robel.dev");
